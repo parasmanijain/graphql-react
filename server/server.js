48fcfe4887c23f72fb3418184f4311b36dccd85d
Paras Mani Jain -X (parasmja - HCL TECHNOLOGIES LIMITED at Cisco) <parasmja@partnet.cisco.com>
2020-08-30 23:05:24 +0530

    path updated

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, './client/public')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.get('*', function (_, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'), function (
+  res.sendFile(path.join(__dirname, '../client/public/index.html'), function (
     err
   ) {
     if (err) {
